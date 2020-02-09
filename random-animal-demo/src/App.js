3dd98c59542c0cb083802db871fdf66a1d8c10e0
Danny Perez <danny@intricatecloud.io>
2020-02-09 17:23:36 -0500

    Adding axios to call the apis, and showing a different image every few seconds

--- random-animal-demo/src/App.js
+++ random-animal-demo/src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      index: 0,
+      currentAnimal: null
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const index = this.state.index
+      let newIndex
+      if (index > 2) {
+        newIndex = 0
+      } else {
+        newIndex = index + 1
+      }
+      this.setState({index: newIndex})
+
+      const DOG = 0
+      const CAT = 1
+      const GOAT = 2
+
+      switch(newIndex) {
+        case CAT:
+          axios.get('https://aws.random.cat/meow').then((response) => {
+            const imageSrc = response.data.file
+            const text = 'CAT'
+            this.setState({currentAnimal: {imageSrc, text}})
+          })
+          break;
+        case DOG:
+          axios.get('https://random.dog/woof.json').then((response) => {
+            const imageSrc = response.data.url
+            const text = 'DOG'
+            this.setState({currentAnimal: {imageSrc, text}})
+          })
+          break;
+        case GOAT:
+          const imageSrc = 'http://placegoat.com/200'
+          const text = 'GOAT'
+          this.setState({currentAnimal: {imageSrc, text}})
+          break;
+      }
+    }, 3000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  render() {
+    const mainElementWithImageSrc = (imageSrc, text) => {
+      return (
+        <div>
+          <img src={imageSrc} className="App-logo" alt="logo" />
+          <p>
+            {text}
+          </p>
+        </div>
+      )
+    }
+    let element;
+    const currentAnimal = this.state.currentAnimal
+    if (currentAnimal) {
+      element = mainElementWithImageSrc(currentAnimal.imageSrc, currentAnimal.text)
+    } else {
+      element = (<div>Loading....</div>)
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {element}
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
